9122d6512fd92e98203b1ee08783cd091af8032a
Daniel de Andrade Varela <daniel.andrade.varela@gmail.com>
2021-04-29 23:09:14 -0300

    clear on resize removed to permit the new stageStack.ts to hold it

--- src/runner/Init.ts
+++ src/runner/Init.ts
@@ -2,5 +2,4 @@ import "./Keyboard"
 import { CursorController } from "../interface/CursorController"
 
 CursorController.hide().clearScreen()
-process.stdout.on("resize", () => CursorController.clearScreen())
 process.on('exit', () => CursorController.clearScreen().unhide())
