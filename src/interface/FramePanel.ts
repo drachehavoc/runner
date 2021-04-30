a69a0ac607d0489b9fbb2546d711bb5f1d30dd06
Daniel de Andrade Varela <daniel.andrade.varela@gmail.com>
2021-04-29 23:17:25 -0300

    init

--- src/interface/FramePanel.ts
+++ src/interface/FramePanel.ts
@@ -1,3 +1,22 @@
+import { Box } from "./Box";
+import { CursorController } from "./CursorController";
+
 export class FramePanel {
+    private _content: string[] = []
+    private _cursor = new CursorController()
+    private _size = { vertical: 0, horizontal: 0 }
     
+    constructor(
+        private _box: Box
+    ) {
+        this._cursor
+            .move(_box.topLeft)
+            .write('A')
+            .move(_box.bottomRight)
+            .write('B')
+    }
+
+    addContent(content: string) {
+
+    }
 }
