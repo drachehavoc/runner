3a2a8d27882c89f77870898126b7f9b59361b760
Daniel de Andrade Varela <daniel.andrade.varela@gmail.com>
2021-04-29 23:02:39 -0300

    stack for controll order of redraw on terminal resize

---
+++ src/interface/clearStack.ts
@@ -0,0 +1,9 @@
+import { CursorController } from "./CursorController"
+
+const clearScreenFns: Set<Function> = new Set()
+clearScreenFns.add(() => CursorController.clearScreen())
+process.stdout.on("resize", () => clearScreenFns.forEach(fn => fn()))
+
+export const clearStack = {
+    add: clearScreenFns.add.bind(clearScreenFns)
+}
